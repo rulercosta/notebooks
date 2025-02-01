83b7c4abc9bfb65c6ec4e0dbe8ca237b5756abc3
rulercosta <rulercosta.mail@gmail.com>
2025-02-01 12:00:00 +0530

    Update compile script to remove additional SVG files during cleanup

--- Linear Algebra/compile.sh
+++ Linear Algebra/compile.sh
@@ -2,4 +2,4 @@
 pdflatex -shell-escape main.tex
 pdflatex -shell-escape main.tex
 # Clean up auxiliary files if desired
-rm -f *.aux *.log *.toc *.out *.lof *.lot *.fls *.fdb_latexmk *.synctex.gz
+rm -rf *.aux *.log *.toc *.out *.lof *.lot *.fls *.fdb_latexmk *.synctex.gz svg-inkscape
