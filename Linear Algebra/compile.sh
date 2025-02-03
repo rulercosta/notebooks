f1079555c49e4d57de7a06dfa23eb6059af7a710
rulercosta <rulercosta.mail@gmail.com>
2025-02-03 12:00:00 +0530

    Update compile script to use LinearAlgebra.tex instead of main.tex

--- Linear Algebra/compile.sh
+++ Linear Algebra/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pdflatex -shell-escape main.tex
-pdflatex -shell-escape main.tex
+pdflatex -shell-escape LinearAlgebra.tex
+pdflatex -shell-escape LinearAlgebra.tex
 # Clean up auxiliary files if desired
 rm -rf *.aux *.log *.toc *.out *.lof *.lot *.fls *.fdb_latexmk *.synctex.gz svg-inkscape
